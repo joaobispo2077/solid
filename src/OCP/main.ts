6a05d1163688df5f62b6e4d97ddd7ff392056e88
João Bispo <joaobispo2077@gmail.com>
2021-05-22 18:08:53 -0300

    Open Closed Principle

---
+++ src/OCP/main.ts
@@ -0,0 +1,36 @@
+import { Messaging } from './services/Messaging';
+import { ShoppingCart } from './classes/ShoppingCart';
+import { Order } from './classes/Order';
+import { Product } from './classes/Product';
+import { OrderRepositories } from './services/OrderRepositories';
+import { FiftyPercentDiscount } from './classes/Discount';
+
+const discount = new FiftyPercentDiscount();
+const shoppingCart = new ShoppingCart(discount);
+const messaging = new Messaging();
+const orderRepositories = new OrderRepositories();
+
+const order = new Order(shoppingCart, messaging, orderRepositories);
+
+const product = new Product('water bootle', 15);
+const item = new Product('Playstation 5', 5000);
+
+console.log(order.orderStatus);
+
+shoppingCart.addItem(product);
+shoppingCart.addItem(item);
+shoppingCart.addItem(item);
+
+console.log(shoppingCart.totalItems);
+console.log(shoppingCart.items);
+
+console.log(shoppingCart.getTotalPrice());
+console.log(shoppingCart.getTotalPriceWithDiscount());
+shoppingCart.removeItem(2);
+
+console.log(shoppingCart.items);
+
+order.checkout();
+
+console.log(shoppingCart.items);
+console.log(order.orderStatus);
