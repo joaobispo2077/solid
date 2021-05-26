aa8c4daec142754c53d67eaf23cee2b55a24ff2d
João Bispo <joaobispo2077@gmail.com>
2021-05-25 21:14:44 -0300

    Tradeoffs: DRY, KISS and YAGNI

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
 console.log('hi');
+/*
+ Vantagens e desvantagens do S.O.L.I.D.
+
+  Vantagens:
+    - Código modular.
+    - Código reutilizável (DRY - Don't repeat yourself).
+    - Código testável.
+    - Baixo acoplamento e alta coesão.
+    - Código expansível.
+    - Separations of concerns (Separação de conceitos)
+    - Fácil manutenção.
+
+  Desvantagens:
+    - Complexidade.
+    - Quantidade de código aumenta.
+    - YAGNI (You aren't gonna need it).
+    - KISS (Keep it Simple, Stupid!).
+
+
+
+
+*/
